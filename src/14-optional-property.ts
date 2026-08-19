5d6f6c8fbb0d815fcf80d774299736c55ae48a1c
Aminul islam <aminulislam424842@gmail.com>
2026-08-19 21:19:46 +0600

    Complete optional property task

---
+++ src/14-optional-property.ts
@@ -0,0 +1,17 @@
+type User = {
+  name: string,
+  age: number,
+  phone?: number
+}
+const user:User = {
+  name: "Aminul",
+  age: 21,
+  phone: 1709150885
+}
+const user1:User = {
+  name: "Aminul",
+  age: 21,
+}
+
+console.log(user)
+console.log(user1)
