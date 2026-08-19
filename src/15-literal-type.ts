956325a1dfde2ff93041f881038344098af47c7c
Aminul islam <aminulislam424842@gmail.com>
2026-08-19 21:39:01 +0600

    Complete literal type task

---
+++ src/15-literal-type.ts
@@ -0,0 +1,16 @@
+let role: "admin" | "user";
+
+role = "admin"
+// console.log(role)
+
+
+let conformation: "pending" | "success" | "failed";
+conformation = "success"
+
+// console.log(conformation)
+
+
+let direction: "up" | "down" | "left" | "right";
+direction = "left"
+
+// console.log(direction)
