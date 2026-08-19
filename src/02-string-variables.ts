2f2515a8937fbffa40cd7ca3febbca5efc30354d
Aminul islam <aminulislam424842@gmail.com>
2026-08-19 14:51:08 +0600

    Add TypeScript string interpolation example

---
+++ src/02-string-variables.ts
@@ -0,0 +1,5 @@
+const firstName: string = "Aminul Islam"
+const lastName: string = "Mahi"
+const country: string = "Bangladesh";
+
+console.log(`My name is ${firstName} ${lastName}. My Country name is ${country}`)
