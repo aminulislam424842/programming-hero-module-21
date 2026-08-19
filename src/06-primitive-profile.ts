d0d4a2fcbea55853c453235bc45bdbe41ca9d3cc
Aminul islam <aminulislam424842@gmail.com>
2026-08-19 15:52:06 +0600

    Complete primitive profile task

---
+++ src/06-primitive-profile.ts
@@ -0,0 +1,10 @@
+const name: string = "Aminul islam mahi"
+const age: number = 21
+const isStudent: boolean = true
+const country: string = "Bangladesh"
+
+// console.log(name,age,isStudent,country)
+console.log(`Name : ${name}
+Age : ${age}
+Student : ${isStudent}
+Country : ${country}`)
